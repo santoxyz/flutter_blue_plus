eaadb81cdd207e3fd18ebc0855300764bbc821da
Luca Santini <luca.santini@phoenix-factory.it>
2024-04-17 18:31:07 +0200

    bloody rebase over recorder branch completed

--- lib/src/flutter_blue_plus.dart
+++ lib/src/flutter_blue_plus.dart
@@ -10,10 +10,11 @@ class FlutterBluePlus {
   //
 
   static bool _initialized = false;
+
   /// Singleton boilerplate
   FlutterBluePlus._() {
-    _channel.setMethodCallHandler((MethodCall call) async {
-      _methodStreamController.add(call);
+    _methods.setMethodCallHandler((MethodCall call) async {
+      _methodStream.add(call);
     });
     if (!Platform.isWindows) {
       setLogLevel(logLevel);
